4a0c03e29a937cc1c94f5491b3a63ff1c76762c7
MK <michal@toke.com>
2015-11-03 16:20:13 +0100

    sprawdzanie portu dziala

--- site_portchecker/Program.cs
+++ site_portchecker/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,26 @@ namespace site_portchecker
     {
         static void Main(string[] args)
         {
+
+            IPHostEntry adresWWW = Dns.GetHostEntry("www.github.com");
+            IPAddress adresIP = adresWWW.AddressList[0];
+            System.Net.Sockets.Socket socket = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(adresIP, 80);
+                if (socket.Connected == true)
+                    Console.WriteLine("Port jest otwarty");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Port jest zamkniety");
+            }
+            finally
+            {
+                socket.Close();
+            }
+            Console.ReadKey();
+
         }
     }
 }
