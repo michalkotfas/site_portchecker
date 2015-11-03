11aa5ffdb3e97495e1583305fd100a3f1a30265c
MK <michal@toke.com>
2015-11-03 16:18:25 +0100

    initial commit

---
+++ site_portchecker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace site_portchecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
